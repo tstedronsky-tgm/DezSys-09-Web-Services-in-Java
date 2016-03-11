661aa7bb4d584357172d37ab6c7ce280087f74fa
Thomas Stedronsky <tstedronsky@student.tgm.ac.at>
2016-03-11 09:03:49 +0100

    start

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
